35895dac1e536943d9ab0c4d89473afaa21259f6
Brandon Randle <branr62@gmail.com>
2025-02-16 16:29:24 +0000

    switch to probe-rs

--- src/main.rs
+++ src/main.rs
@@ -4,19 +4,17 @@
 
 //!TODO: Report more things like uptime, wifi strength
 
-use core::panic::PanicInfo;
-
 use cortex_m::delay::Delay;
 use cyw43::JoinOptions;
 use cyw43_pio::{PioSpi, DEFAULT_CLOCK_DIVIDER};
 
+use defmt::{info, warn};
 use embassy_executor::Spawner;
 use embassy_sync::blocking_mutex::raw::ThreadModeRawMutex;
 use embassy_time::{Duration, Timer, WithTimeout};
 use embedded_graphics::pixelcolor::Rgb565;
 use embedded_graphics::prelude::{IntoStorage, RgbColor};
 use embedded_hal_1::delay::DelayNs;
-use log::{error, info, warn};
 use rand::RngCore;
 
 use embassy_net::{Config, StackResources};
@@ -24,10 +22,11 @@ use embassy_rp::bind_interrupts;
 use embassy_rp::clocks::{clk_sys_freq, RoscRng};
 use embassy_rp::gpio::{Level, Output};
 use embassy_rp::i2c::InterruptHandler as I2cInterruptHandler;
-use embassy_rp::peripherals::{DMA_CH0, I2C0, I2C1, PIO0, PIO1, USB};
+use embassy_rp::peripherals::{DMA_CH0, I2C0, I2C1, PIO0, PIO1};
 use embassy_rp::pio::{InterruptHandler, Pio};
 use embassy_rp::spi::{self, Spi};
-use embassy_rp::usb::{Driver, InterruptHandler as UsbInterruptHandler};
+
+use {defmt_rtt as _, panic_probe as _};
 
 use sen55::Readings;
 use st7789v2_driver::ST7789V2;
@@ -42,7 +41,6 @@ mod sen55;
 bind_interrupts!(struct Irqs {
     PIO0_IRQ_0 => InterruptHandler<PIO0>;
     PIO1_IRQ_0 => InterruptHandler<PIO1>;
-    USBCTRL_IRQ => UsbInterruptHandler<USB>;
     I2C1_IRQ => I2cInterruptHandler<I2C1>;
     I2C0_IRQ => I2cInterruptHandler<I2C0>;
 });
@@ -55,17 +53,9 @@ static MQTT_WORKING_BUFFER: StaticCell<[u8; 8192]> = StaticCell::new();
 static SHARED_CHANNEL: embassy_sync::channel::Channel<ThreadModeRawMutex, Readings, 10> =
     embassy_sync::channel::Channel::new();
 
-#[panic_handler]
-fn handle_panic(info: &PanicInfo) -> ! {
-    // This probably won't get written because the logger is its own task, and we're about to reset.
-    error!("Panicking: {}", info);
-
-    // Not with a bang, but with a whimper.
-    cortex_m::peripheral::SCB::sys_reset();
-}
-
 #[embassy_executor::main]
 async fn main(spawner: Spawner) {
+    // General setup
     let p = embassy_rp::init(Default::default());
     let core = cortex_m::Peripherals::take().unwrap();
 
@@ -74,15 +64,9 @@ async fn main(spawner: Spawner) {
     let fw = include_bytes!("../cyw43-firmware/43439A0.bin");
     let clm = include_bytes!("../cyw43-firmware/43439A0_clm.bin");
 
-    let driver = Driver::new(p.USB, Irqs);
-    spawner.spawn(logger_task(driver)).unwrap();
-
-    // Wait for USB serial to be ready (or for the other end to start listening, not sure, just know it's needed)
-    Timer::after_secs(4).await;
-    info!("USB serial logging up");
+    info!("Hello world!");
 
     // Set up the delay for the first core
-
     let mut delay = Delay::new(core.SYST, clk_sys_freq());
 
     // Grab pins for the i2c to the SEN55 sensor.
@@ -108,10 +92,10 @@ async fn main(spawner: Spawner) {
     // Display pins
     let display_clk = p.PIN_18; // GP18 -> CLK
     let display_mosi = p.PIN_19; // GP19 -> DIN
-    let display_dc = Output::new(p.PIN_16, Level::Low);
-    let display_rst = Output::new(p.PIN_21, Level::Low);
-    let display_cs = Output::new(p.PIN_17, Level::High);
-    let _display_bl = Output::new(p.PIN_22, Level::High);
+    let display_dc = Output::new(p.PIN_16, Level::Low); // GP16 -> DC
+    let display_rst = Output::new(p.PIN_21, Level::Low); // GP21 -> RST
+    let display_cs = Output::new(p.PIN_17, Level::High); // GP17 -> CS (assuming we only have one thing on the bus)
+    let _display_bl = Output::new(p.PIN_22, Level::High); // GP22 -> BL (backlight, always on for now)
 
     let display_spi = Spi::new_blocking_txonly(p.SPI0, display_clk, display_mosi, display_spi_cfg);
 
@@ -124,7 +108,7 @@ async fn main(spawner: Spawner) {
         display_dc,
         display_cs,
         display_rst,
-        true,
+        true, // 'is it rgb?' (yes)
         screen_direction,
         lcd_width,
         lcd_height,
@@ -132,17 +116,14 @@ async fn main(spawner: Spawner) {
 
     let mut delay_wrapper = DelayWrapper::new(&mut delay);
 
+    // Reset and then initialise the display
     display.hard_reset(&mut delay_wrapper).unwrap();
-
     Timer::after_millis(500).await;
-
     display.init(&mut delay_wrapper).unwrap();
+    display.clear_screen(Rgb565::BLACK.into_storage()).unwrap(); //TODO: be less green and ugly
 
-    display.clear_screen(Rgb565::GREEN.into_storage()).unwrap();
-
-    Timer::after_secs(5).await;
-
-    // Grab pins for the CYW43
+    // Grab pins for the CYW43 (wifi chip); set up SPI to it.
+    // Wifi chip is integrated into the pico and we use PIO to drive SPI to it.
     let pwr = Output::new(p.PIN_23, Level::Low);
     let cs = Output::new(p.PIN_25, Level::High);
     let mut pio = Pio::new(p.PIO0, Irqs);
@@ -157,7 +138,7 @@ async fn main(spawner: Spawner) {
         p.DMA_CH0,
     );
 
-    // Start the CYW43 driver (the wifi chip)
+    // Start the CYW43 driver
     static STATE: StaticCell<cyw43::State> = StaticCell::new();
     let state = STATE.init(cyw43::State::new());
     let (net_device, mut control, runner) = cyw43::new(state, pwr, spi, fw).await;
@@ -172,7 +153,7 @@ async fn main(spawner: Spawner) {
 
     let config = Config::dhcpv4(Default::default());
 
-    // Generate random seed
+    // Generate random seed super securely
     let seed = rng.next_u64();
 
     // Init network stack
@@ -215,12 +196,6 @@ async fn main(spawner: Spawner) {
     }
 }
 
-/// Pipes log messages over USB serial back to whatever host just flashed us.
-#[embassy_executor::task]
-async fn logger_task(driver: Driver<'static, USB>) {
-    embassy_usb_logger::run!(1024, log::LevelFilter::Info, driver);
-}
-
 /// Pokes the CYW43 driver to do hardware network stuff.
 #[embassy_executor::task]
 async fn cyw43_task(
@@ -250,7 +225,7 @@ async fn wait_for_network(control: &mut cyw43::Control<'_>, stack: &embassy_net:
         {
             Ok(_) => break,
             Err(err) => {
-                warn!("wifi join failed with status: {err:?}");
+                warn!("wifi join failed with status: {}", err);
             }
         }
     }
@@ -292,7 +267,7 @@ impl<'a> DelayWrapper<'a> {
     }
 }
 
-impl<'a> DelayNs for DelayWrapper<'a> {
+impl DelayNs for DelayWrapper<'_> {
     fn delay_ns(&mut self, ns: u32) {
         let us = (ns + 999) / 1000; // Convert nanoseconds to microseconds
         self.delay.delay_us(us); // Use microsecond delay
